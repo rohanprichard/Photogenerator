4ca104eb79aff47fa2542f16a7e99b9b0eb44855
rohanprichard <rohanprichard@gmail.com>
2022-05-31 12:05:24 +0530

    Made the app compatible with vertical and horizontal images

--- Photogenerator/ViewController.swift
+++ Photogenerator/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        button.frame = CGRect(x: 30, y: view.frame.size.height - 150 - view.safeAreaInsets.bottom , width: view.frame.size.width - 60, height: 60)
+        button.frame = CGRect(x: 30, y: view.frame.size.height - 50 - view.safeAreaInsets.bottom , width: view.frame.size.width - 60, height: 60)
     }
     
     override func viewDidLoad() {
@@ -41,9 +41,8 @@ class ViewController: UIViewController {
         view.backgroundColor = .systemCyan
         view.addSubview(imageView)
         view.addSubview(button)
-        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.frame = CGRect(x: 0, y: 0, width: 360, height: 640)
         imageView.center = view.center
-        button.frame = CGRect(x: 20, y: view.frame.size.height - 50 - view.safeAreaInsets.bottom , width: view.frame.size.width - 40, height: 50)
         getRandomPhoto()
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         // Do any additional setup after loading the view.
@@ -51,7 +50,7 @@ class ViewController: UIViewController {
     
     func getRandomPhoto()
     {
-        let urlstr = "https://source.unsplash.com/random/600x600"
+        let urlstr = "https://source.unsplash.com/random/1080x1920"
         let url = URL(string: urlstr)!
         guard let data = try? Data(contentsOf: url) else{
             return
